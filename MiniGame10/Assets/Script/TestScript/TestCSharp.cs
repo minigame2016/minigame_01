af6cfcb9b9c319686cf4981ca02fd8336ecdda79
minigame2016@163 <minigame2016@163.com>
2016-08-23 21:33:42 +0800

    测试修改

--- MiniGame10/Assets/Script/TestScript/TestCSharp.cs
+++ MiniGame10/Assets/Script/TestScript/TestCSharp.cs
@@ -5,7 +5,7 @@ public class TestCSharp : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        Debug.LogWarning("-------------------TestMiniGame10");
+        Debug.LogWarning("TestMiniGame10");
 	}
 	
 	// Update is called once per frame
